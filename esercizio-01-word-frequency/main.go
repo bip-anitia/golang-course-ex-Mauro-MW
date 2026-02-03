d372cc2d391f0450f17f4193f05ab6427cbce712
Marco Widesott <marco.widesott@anitia.it>
2026-02-03 09:56:48 +0100

    init ex 1

--- esercizio-01-word-frequency/main.go
+++ esercizio-01-word-frequency/main.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"unicode"
 )
 
+type WordCount struct {
+	Word  string
+	Count int
+}
+
 func main() {
-	// TODO: Implementare il word frequency counter
-	fmt.Println("Word Frequency Counter")
+	counts := make(map[string]int)
+
+	if len(os.Args) > 1 {
+		for _, filename := range os.Args[1:] {
+			f, err := os.Open(filename)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "errore apertura file:", err)
+				continue
+			}
+			if err := countLines(f, counts); err != nil {
+				fmt.Fprintln(os.Stderr, "errore lettura file:", err)
+			}
+			f.Close()
+		}
+	} else {
+		if err := countLines(os.Stdin, counts); err != nil {
+			fmt.Fprintln(os.Stderr, "errore lettura stdin:", err)
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) error {
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.ToLower(scanner.Text())
+		words := strings.FieldsFunc(line, func(r rune) bool {
+			return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+		})
+		for _, w := range words {
+			if w != "" {
+				counts[w]++
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
 }
