3faa0ed8f7d9c7beeaf6c7c78a19faeb674afe1c
Marco Widesott <marco.widesott@anitia.it>
2026-02-04 17:51:24 +0100

    init ex 6

--- esercizio-06-worker-pool/main.go
+++ esercizio-06-worker-pool/main.go
@@ -2,7 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+type Task struct {
+	ID      int
+	Data    interface{}
+	Process func(interface{}) (interface{}, error)
+}
+
+type Result struct {
+	TaskID int
+	Value  interface{}
+	Error  error
+}
+
+type WorkerPool struct {
+	numWorkers int
+	tasks      chan Task
+	results    chan Result
+	done       chan struct{}
+	wg         sync.WaitGroup
+}
 
 func main() {
 	// TODO: Implementare il worker pool
