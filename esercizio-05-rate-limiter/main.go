b93fbbabf9eefe14f7dcc0e759e62eae37f42db1
Marco Widesott <marco.widesott@anitia.it>
2026-02-04 16:07:38 +0100

    init ex 5

--- esercizio-05-rate-limiter/main.go
+++ esercizio-05-rate-limiter/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -13,8 +16,48 @@ type TokenBucketLimiter struct {
 }
 
 func main() {
-	// TODO: Implementare il rate limiter
-	fmt.Println("Rate Limiter")
+	rate := flag.Int("rate", 5, "requests per second")
+	workers := flag.Int("workers", 10, "number of workers")
+	duration := flag.Duration("duration", 10*time.Second, "test duration")
+	flag.Parse()
+
+	if *rate <= 0 || *workers <= 0 || *duration <= 0 {
+		fmt.Println("invalid config")
+		return
+	}
+	fmt.Println("Rate Limiter Test")
+	fmt.Printf("Rate: %d req/s\n", *rate)
+	fmt.Printf("Workers: %d\n", *workers)
+	fmt.Printf("Duration: %s\n", duration.String())
+	fmt.Println("Starting test...")
+
+	refill := time.Second / time.Duration(*rate)
+	limiter := NewTokenBucketLimiter(*rate, refill)
+	defer limiter.Stop()
+
+	var total int64
+	done := time.After(*duration)
+	var wg sync.WaitGroup
+	wg.Add(*workers)
+	for i := 0; i < *workers; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-done:
+					return
+				default:
+					limiter.Wait()
+					atomic.AddInt64(&total, 1)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+
+	fmt.Printf("Total requests: %d\n", total)
+	fmt.Printf("Actual rate: %.2f req/s\n", float64(total)/duration.Seconds())
+
 }
 
 func NewTokenBucketLimiter(maxTokens int, refillRate time.Duration) *TokenBucketLimiter {
