5d112c659e66a0c3e6ff2987e2b48685c99d362e
Marco Widesott <marco.widesott@anitia.it>
2026-02-05 11:26:02 +0100

    init func start && worker

--- esercizio-06-worker-pool/main.go
+++ esercizio-06-worker-pool/main.go
@@ -25,6 +25,35 @@ type WorkerPool struct {
 	wg         sync.WaitGroup
 }
 
+func NewWorkerPool(n int) *WorkerPool {
+	return &WorkerPool{
+		numWorkers: n,
+		tasks:      make(chan Task, n),
+		results:    make(chan Result, n),
+		done:       make(chan struct{}),
+	}
+}
+
+func (wp *WorkerPool) Start() {
+	for i := 0; i < wp.numWorkers; i++ {
+		wp.wg.Add(1)
+		go wp.worker(i)
+	}
+}
+
+func (wp *WorkerPool) worker(id int) {
+	defer wp.wg.Done()
+	defer func() {
+		if r := recover(); r != nil {
+			wp.results <- Result{TaskID: -1, Error: fmt.Errorf("panic: %v", r)}
+		}
+	}()
+	for task := range wp.tasks {
+		val, err := task.Process(task.Data)
+		wp.results <- Result{TaskID: task.ID, Value: val, Error: err}
+	}
+}
+
 func main() {
 	// TODO: Implementare il worker pool
 	fmt.Println("Worker Pool Pattern")
