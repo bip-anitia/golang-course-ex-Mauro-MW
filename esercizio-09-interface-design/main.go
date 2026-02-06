5ae4945fb70086243c76fcb8e30d5cb2fe299e6c
Marco Widesott <marco.widesott@anitia.it>
2026-02-06 12:44:39 +0100

    init ex 9

--- esercizio-09-interface-design/main.go
+++ esercizio-09-interface-design/main.go
@@ -1,8 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"sync"
 )
+
+var ErrNotFound = errors.New("key not found")
+
+type Storage interface {
+	Get(key string) ([]byte, error)
+	Put(key string, value []byte) error
+	Delete(key string) error
+	List() ([]string, error)
+	Close() error
+}
+
+type MemoryStorage struct {
+	mu   sync.RWMutex
+	data map[string][]byte
+}
+
+type CachedStorage struct {
+	backend Storage
+	cache   map[string][]byte
+	mu      sync.RWMutex
+}
 
 func main() {
 	// TODO: Implementare il sistema di storage con interfacce
